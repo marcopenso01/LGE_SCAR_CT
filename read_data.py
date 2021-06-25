529acf82772a6d527bbbf2508661f3b1627e36dd
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2021-06-25 16:34:11 +0200

    Update read_data.py

--- read_data.py
+++ read_data.py
@@ -5,5 +5,21 @@ import os
 import numpy as np
 import logging
 import h5py
+import scipy.io
 from skimage import transform
 
+logging.basicConfig(
+    level=logging.INFO # allow DEBUG level messages to pass through the logger
+    )
+
+def makefolder(folder):
+    '''
+    Helper function to make a new folder if doesn't exist
+    :param folder: path to new folder
+    :return: True if folder created, False if folder already exists
+    '''
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+        return True
+    return False
+  
