d4e1d3c6da3cf635de896b2f9bc719850354866d
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2022-01-19 15:06:59 +0100

    Update pre_proc.py

--- pre_proc.py
+++ pre_proc.py
@@ -33,36 +33,135 @@ def standardize_image(image, mask):
     
 path = r'F:/CT-tesi/Pre-proc'
 
-paz = []
-segs = []
-out = []
+n_paz = len(os.listdir(path))
+ind_paz = np.arange(n_paz)
+random.Random(4).shuffle(ind_paz)
 
-for paz in os.listdir(path):
-    data = h5py.File(os.path.join(path, paz, 'tesi_tac.hdf5'), "r")
-
-    #data.keys()
-    n_file = len(data['myo'])
+# ciclo su fold (5-fold)
+for b_i in range(5):
     
-    random_indices = np.arange(n_file)
-    np.random.shuffle(random_indices)
-    count_1 = 0
-    count_0 = 0
+    print('-------------------------------------')
+    print('fold: %s' % b_i)
     
-    for i in range(n_file):
-        if data['scar_area'][i] > 10:
-            img = standardize_image(data['seg_cropped'][i], data['mask_seg_cropped'][i])
-            segs.append(img)
-            out.append(1)
-            paz.append(data['paz'][i])
-            count_1 += 1
-    for i in range(n_file):
-        if data['scar_area'][random_indices[i]] == 0:
-            img = standardize_image(data['seg_cropped'][random_indices[i]], data['mask_seg_cropped'][random_indices[i]])
-            segs.append()
-            out.append(0)
-            paz.append(data['paz'][random_indices[i]])
-            count_0 += 1
-        if count_0 >= count_1:
-            break
-            
-    data.close()
+    test_ind = ind_paz[b_i*10:b_i*10+10]
+    tr = []
+    train_ind = []
+    val_ind = []
+    for ii in range(n_paz):
+        if ii < b_i*10 or ii >= b_i*10+10:
+            tr.append(ind_paz[ii])
+    
+    rd_ind = np.arange(len(tr))
+    random.Random(1).shuffle(rd_ind)
+    for ii in range(len(rd_ind)):
+        if ii<5:
+            val_ind.append(tr[rd_ind[ii]])
+        else:
+            train_ind.append(tr[rd_ind[ii]])
+    
+    
+    paz_test = []
+    segs_test = []
+    out_test = []
+    paz_tr = []
+    segs_tr = []
+    out_tr = []
+    paz_val = []
+    segs_val = []
+    out_val = []
+    
+    # test patients
+    for zz in range(len(test_ind)):
+        
+        print('processing test patients: %s' % os.listdir(path)[test_ind[zz]])
+        data = h5py.File(os.path.join(path, os.listdir(path)[test_ind[zz]], 'tesi_tac.hdf5'), "r")
+        #data.keys()
+        n_file = len(data['myo'])
+        
+        random_indices = np.arange(n_file)
+        np.random.shuffle(random_indices)
+        count_1 = 0
+        count_0 = 0
+        
+        for i in range(n_file):
+            if data['scar_area'][i] > 10:
+                img = standardize_image(data['seg_cropped'][i], data['mask_seg_cropped'][i])
+                segs.append(img)
+                out.append(1)
+                paz.append(data['paz'][i])
+                count_1 += 1
+        for i in range(n_file):
+            if data['scar_area'][random_indices[i]] == 0:
+                img = standardize_image(data['seg_cropped'][random_indices[i]], data['mask_seg_cropped'][random_indices[i]])
+                segs.append(img)
+                out.append(0)
+                paz.append(data['paz'][random_indices[i]])
+                count_0 += 1
+            if count_0 >= count_1:
+                break
+                
+        data.close()
+    
+    # train patients
+    for zz in range(len(train_ind)):
+        
+        print('processing train patients: %s' % os.listdir(path)[test_ind[zz]])
+        data = h5py.File(os.path.join(path, os.listdir(path)[test_ind[zz]], 'tesi_tac.hdf5'), "r")
+        #data.keys()
+        n_file = len(data['myo'])
+        
+        random_indices = np.arange(n_file)
+        np.random.shuffle(random_indices)
+        count_1 = 0
+        count_0 = 0
+        
+        for i in range(n_file):
+            if data['scar_area'][i] > 10:
+                img = standardize_image(data['seg_cropped'][i], data['mask_seg_cropped'][i])
+                segs_tr.append(img)
+                out_tr.append(1)
+                paz_tr.append(data['paz'][i])
+                count_1 += 1
+        for i in range(n_file):
+            if data['scar_area'][random_indices[i]] == 0:
+                img = standardize_image(data['seg_cropped'][random_indices[i]], data['mask_seg_cropped'][random_indices[i]])
+                segs_tr.append(img)
+                out_tr.append(0)
+                paz_tr.append(data['paz'][random_indices[i]])
+                count_0 += 1
+            if count_0 >= count_1:
+                break
+                
+        data.close()
+        
+    # val patients
+    for zz in range(len(val_ind)):
+        
+        print('processing val patients: %s' % os.listdir(path)[test_ind[zz]])
+        data = h5py.File(os.path.join(path, os.listdir(path)[test_ind[zz]], 'tesi_tac.hdf5'), "r")
+        #data.keys()
+        n_file = len(data['myo'])
+        
+        random_indices = np.arange(n_file)
+        np.random.shuffle(random_indices)
+        count_1 = 0
+        count_0 = 0
+        
+        for i in range(n_file):
+            if data['scar_area'][i] > 10:
+                img = standardize_image(data['seg_cropped'][i], data['mask_seg_cropped'][i])
+                segs_val.append(img)
+                out_val.append(1)
+                paz_val.append(data['paz'][i])
+                count_1 += 1
+        for i in range(n_file):
+            if data['scar_area'][random_indices[i]] == 0:
+                img = standardize_image(data['seg_cropped'][random_indices[i]], data['mask_seg_cropped'][random_indices[i]])
+                segs_val.append(img)
+                out_val.append(0)
+                paz_val.append(data['paz'][random_indices[i]])
+                count_0 += 1
+            if count_0 >= count_1:
+                break
+                
+        data.close()
