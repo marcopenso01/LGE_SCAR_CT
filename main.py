bdd2f73c1ee059e8641915df19aeb48d800e4c2d
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2022-01-20 12:24:31 +0100

    Update and rename train.py to main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import os
 import h5py
 import tensorflow as 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 
 directory = 'F:\CT-tesi\data'
 data = h5py.File(os.path.join(directory, 'tac_fold0.hdf5'), 'r')
@@ -37,8 +38,18 @@ train_generator = train_datagen.flow(train_images, train_labels, batch_size=batc
 valid_generator = train_datagen.flow(val_images, val_labels, batch_size=batch_size)
 test_generator = test_datagen.flow(test_images, batch_size=1)
 
+callbacks=[EarlyStopping(patience=15,verbose=1),\
+            ReduceLROnPlateau(factor=0.1, patience=5, min_lr=0.000001,verbose=1),\
+                ModelCheckpoint('modelName.h5'.format(epochs),verbose=1, save_best_only=True,\
+                                save_weights_only=False)
+
 model = get_model()
 print('training started...')
-history = model.fit(train_generator, epochs = 100, validation_data = valid_generator, verbose = 1, callbacks=[tensorboard])
+history = model.fit(train_generator, epochs = 100, validation_data = valid_generator, verbose = 1, callbacks=callbacks)
 print('training done...')
 
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+TESTING AND EVALUATING THE MODEL (repeated in the beginning of load.py)
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
