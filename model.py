340fdfb319452c61ac6848f5ccb268fe3911fba8
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2022-01-20 14:48:55 +0100

    Update model.py

--- model.py
+++ model.py
@@ -3,12 +3,17 @@ import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras import Model 
 initializer = tensorflow.keras.initializers.GlorotNormal()
 import os
 
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.applications import ResNet50V2
+from tensorflow.keras.applications import InceptionResNetV2
+
 def get_model():
     model = tf.keras.Sequential()
-    model.add(Conv2D(32, kernel_size = (3, 3), activation='relu', kernel_initializer=initializer, input_shape=(CONST.IMG_SIZE, CONST.IMG_SIZE)))
+    model.add(Conv2D(32, kernel_size = (3, 3), activation='relu', kernel_initializer=initializer, input_shape=(85,85,1)))
     model.add(MaxPooling2D(pool_size=(2,2)))
     model.add(BatchNormalization())
     
@@ -38,6 +43,75 @@ def get_model():
     model.add(Dropout(0.3)) ##
     model.add(Dense(1, activation = 'sigmoid', kernel_initializer=initializer))
 
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics = ['accuracy'])
-    print('model prepared...')
+    return model
+
+
+def VGG16_model():
+    # It should have exactly 3 inputs channels, and width and height should be no smaller than 32.
+    input_tensor_shape = Input(shape=(85, 85, 1))
+    images = Concatenate(axis=-1)([input_tensor_shape, input_tensor_shape, input_tensor_shape])
+    
+    base_model = VGG16(input_tensor = images,
+                       include_top = False,
+                       weights = 'imagenet')
+    base_model.trainable = False
+    
+    x = layers.Flatten()(base_model.output)
+    x = layers.Dense(256, activation='relu',kernel_initializer=initializer)(x)
+    x = layers.Dropout(0.5)(x)
+    x = layers.Dense(128, activation='relu',kernel_initializer=initializer)(x)
+    x = layers.Dropout(0.5)(x)
+    # Add a final sigmoid layer with 1 node for classification output
+    output  = layers.Dense(1, activation='sigmoid')(x)
+
+    model = Model(base_model.input, output, name='VGG16')
+    
+    return model
+
+
+def ResNet50V2_model():
+    # It should have exactly 3 inputs channels, and width and height should be no smaller than 32.
+    input_tensor_shape = Input(shape=(85, 85, 1))
+    images = Concatenate(axis=-1)([input_tensor_shape, input_tensor_shape, input_tensor_shape])
+    
+    #include_top: whether to include the fully-connected layer at the top of the network. 
+    base_model = ResNet50V2(input_tensor = images,
+                            include_top = False,
+                            weights = 'imagenet')
+    base_model.trainable = False
+    
+    x = layers.Flatten()(base_model.output)
+    x = layers.Dense(256, activation='relu',kernel_initializer=initializer)(x)
+    x = layers.Dropout(0.5)(x)
+    x = layers.Dense(128, activation='relu',kernel_initializer=initializer)(x)
+    x = layers.Dropout(0.5)(x)
+    # Add a final sigmoid layer with 1 node for classification output
+    output  = layers.Dense(1, activation='sigmoid')(x)
+
+    model = Model(base_model.input, output, name='VGG16')
+    
+    return model
+
+
+def InceptionResNetV2_model():
+    # It should have exactly 3 inputs channels, and width and height should be no smaller than 75.
+    input_tensor_shape = Input(shape=(85, 85, 1))
+    images = Concatenate(axis=-1)([input_tensor_shape, input_tensor_shape, input_tensor_shape])
+    
+    #include_top: whether to include the fully-connected layer at the top of the network. 
+    base_model = InceptionResNetV2(input_tensor = images,
+                                   include_top = False,
+                                   weights = 'imagenet')
+    base_model.trainable = False
+    
+    x = layers.Flatten()(base_model.output)
+    x = layers.Dense(256, activation='relu',kernel_initializer=initializer)(x)
+    x = layers.Dropout(0.5)(x)
+    x = layers.Dense(128, activation='relu',kernel_initializer=initializer)(x)
+    x = layers.Dropout(0.5)(x)
+    # Add a final sigmoid layer with 1 node for classification output
+    output  = layers.Dense(1, activation='sigmoid')(x)
+
+    model = Model(base_model.input, output, name='VGG16')
+    
     return model
