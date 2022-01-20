ea73475e8ff5424947baffcf0bbd5d3db32087ef
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2022-01-20 12:39:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,23 @@ import tensorflow as
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
 
+def makefolder(folder):
+    '''
+    Helper function to make a new folder if doesn't exist
+    :param folder: path to new folder
+    :return: True if folder created, False if folder already exists
+    '''
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+        return True
+    return False
+
+
 directory = 'F:\CT-tesi\data'
+output_folder = os.path.join(directory, 'fold0')
+if not os.path.exists(output_folder):
+    makefolder(output_folder)
+
 data = h5py.File(os.path.join(directory, 'tac_fold0.hdf5'), 'r')
 train_images = data['segs_tr'][()]
 train_labels = data['out_tr'][()]
@@ -46,10 +62,34 @@ callbacks=[EarlyStopping(patience=15,verbose=1),\
 model = get_model()
 print('training started...')
 history = model.fit(train_generator, epochs = 100, validation_data = valid_generator, verbose = 1, callbacks=callbacks)
-print('training done...')
+print('Model correctly trained and saved')  
 
+plt.figure(figsize=(8, 8))
+plt.grid(False)
+plt.title("Learning curve LOSS", fontsize=25)
+plt.plot(results.history["loss"], label="Loss")
+plt.plot(results.history["val_loss"], label="Validation loss")
+p=np.argmin(results.history["val_loss"])
+plt.plot( p, results.history["val_loss"][p], marker="x", color="r", label="best model")
+plt.xlabel("Epochs", fontsize=16)
+plt.ylabel("Loss", fontsize=16)
+plt.legend();
+plt.savefig(output_folder+'Loss')
 
+plt.figure(figsize=(8, 8))
+plt.grid(False)
+plt.title("Learning curve ACCURACY", fontsize=25)
+plt.plot(results.history["accuracy"], label="Accuracy")
+plt.plot(results.history["val_accuracy"], label="Validation Accuracy")
+plt.plot( p, results.history["val_accuracy"][p], marker="x", color="r", label="best model")
+plt.xlabel("Epochs", fontsize=16)
+plt.ylabel("Accuracy", fontsize=16)
+plt.legend();
+plt.savefig(output_folder+'Accuracy')
 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-TESTING AND EVALUATING THE MODEL (repeated in the beginning of load.py)
+TESTING AND EVALUATING THE MODEL
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+print('-' * 30)
+print('Testing')
+print('Loading saved weights...')
