8160e2349b5e1d010f12054a19ee4c2fe3e16929
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2022-01-31 16:31:04 +0100

    Update plt_mean_auc.py

--- plt_mean_auc.py
+++ plt_mean_auc.py
@@ -75,3 +75,93 @@ plt.ylabel('True Positive Rate')
 plt.title('Receiver Operating Characteristic')
 plt.savefig(os.path.join('D:\TAC_scar\data7', 'AUC'), dpi=1200)
 plt.close()
+
+scores = [metrics.f1_score(lab0, to_labels(pred0, t)) for t in thresholds0]
+ix = np.argmax(scores)
+pred_adj0 = adjusted_classes(pred0, thresholds0[ix])
+CM0 = metrics.confusion_matrix(lab0, pred_adj0)
+
+scores = [metrics.f1_score(lab1, to_labels(pred1, t)) for t in thresholds1]
+ix = np.argmax(scores)
+pred_adj1 = adjusted_classes(pred1, thresholds1[ix])
+CM1 = metrics.confusion_matrix(lab1, pred_adj1)
+
+scores = [metrics.f1_score(lab2, to_labels(pred2, t)) for t in thresholds2]
+ix = np.argmax(scores)
+pred_adj2 = adjusted_classes(pred2, thresholds2[ix])
+CM2 = metrics.confusion_matrix(lab2, pred_adj2)
+
+scores = [metrics.f1_score(lab3, to_labels(pred3, t)) for t in thresholds3]
+ix = np.argmax(scores)
+pred_adj3 = adjusted_classes(pred3, thresholds3[ix])
+CM3 = metrics.confusion_matrix(lab3, pred_adj3)
+
+scores = [metrics.f1_score(lab4, to_labels(pred4, t)) for t in thresholds4]
+ix = np.argmax(scores)
+pred_adj4 = adjusted_classes(pred4, thresholds4[ix])
+CM4 = metrics.confusion_matrix(lab4, pred_adj4)
+
+TN = CM0[0][0]
+FN = CM0[1][0]
+TP = CM0[1][1]
+FP = CM0[0][1]
+PPV1 = (TP / (TP + FP))
+NPV1 = (TN / (FN + TN))
+ACC1 = (TP+TN)/(TN+FN+TP+FP)
+recall1 = (TP / (TP + FN))
+TN = CM1[0][0]
+FN = CM1[1][0]
+TP = CM1[1][1]
+FP = CM1[0][1]
+PPV2 = (TP / (TP + FP))
+NPV2 = (TN / (FN + TN))
+ACC2 = (TP+TN)/(TN+FN+TP+FP)
+recall2 = (TP / (TP + FN))
+TN = CM2[0][0]
+FN = CM2[1][0]
+TP = CM2[1][1]
+FP = CM2[0][1]
+PPV3 = (TP / (TP + FP))
+NPV3 = (TN / (FN + TN))
+ACC3 = (TP+TN)/(TN+FN+TP+FP)
+recall3 = (TP / (TP + FN))
+TN = CM3[0][0]
+FN = CM3[1][0]
+TP = CM3[1][1]
+FP = CM3[0][1]
+PPV4 = (TP / (TP + FP))
+NPV4 = (TN / (FN + TN))
+ACC4 = (TP+TN)/(TN+FN+TP+FP)
+recall4 = (TP / (TP + FN))
+TN = CM4[0][0]
+FN = CM4[1][0]
+TP = CM4[1][1]
+FP = CM4[0][1]
+PPV5 = (TP / (TP + FP))
+NPV5 = (TN / (FN + TN))
+ACC5 = (TP+TN)/(TN+FN+TP+FP)
+recall5 = (TP / (TP + FN))
+
+mean_ppv = np.mean([PPV1, PPV2, PPV3, PPV4, PPV5])
+mean_npv = np.mean([NPV1, NPV2, NPV3, NPV4, NPV5])
+mean_acc = np.mean([ACC1, ACC2, ACC3, ACC4, ACC5])
+mean_recall = np.mean([recall1, recall2, recall3, recall4, recall5])
+std_ppv = np.std([PPV1, PPV2, PPV3, PPV4, PPV5])
+std_npv = np.std([NPV1, NPV2, NPV3, NPV4, NPV5])
+std_acc = np.std([ACC1, ACC2, ACC3, ACC4, ACC5])
+std_recall = np.std([recall1, recall2, recall3, recall4, recall5])
+
+infer_ppv = mean_ppv - (1.96 * std_ppv / np.sqrt(5))
+upper_ppv = mean_ppv + (1.96 * std_ppv / np.sqrt(5))
+infer_npv = mean_npv - (1.96 * std_npv / np.sqrt(5))
+upper_npv = mean_npv + (1.96 * std_npv / np.sqrt(5))
+infer_acc = mean_acc - (1.96 * std_acc / np.sqrt(5))
+upper_acc = mean_acc + (1.96 * std_acc / np.sqrt(5))
+infer_recall = mean_recall - (1.96 * std_recall / np.sqrt(5))
+upper_recall = mean_recall + (1.96 * std_recall / np.sqrt(5))
+
+print("Mean PPV = %0.2f (CI %0.2f-%0.2f)" % (mean_ppv,infer_ppv,upper_ppv))
+print("Mean NPV = %0.2f (CI %0.2f-%0.2f)" % (mean_npv,infer_npv,upper_npv))
+print("Mean ACC = %0.2f (CI %0.2f-%0.2f)" % (mean_acc,infer_acc,upper_acc))
+print("Mean RECALL = %0.2f (CI %0.2f-%0.2f)" % (mean_recall,infer_recall,upper_recall))
+print("Mean AUC = %0.2f (CI %0.2f-%0.2f)" % (mean_auc,infer_CI,upper_CI))
